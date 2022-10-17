2b044392ac6c134d56004c5de2171b17e3ab2b6e
Daniel Macharia <danielhuios@gmail.com>
2022-10-17 12:42:30 +0300

    assignment completed

--- methods.rb
+++ methods.rb
@@ -1,1 +1,132 @@
 # Your code here!
+
+def greet_programmer
+    puts "Hello, programmer! "
+    end
+
+def greet(name)
+    puts "Hello, #{name}! "
+    end
+
+def greet_with_default(name="programmer")
+    puts "Hello, #{name}! "
+    end
+
+def add(num1, num2)
+    num1 + num2
+    end   
+
+def halve(number)
+    if number.class != Integer
+        return nil
+    end
+    number / 2
+    end
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def my_method (param)
+#     puts "Running my method"
+#     param + 1
+# end
+
+# my_method(1)
+
+# def say_hi(name)
+#     puts "Hi there, #{name}!"
+#   end
+  
+#   say_hi "James"
+
+
+# def greetings(name = "coder")
+#     puts "Hello #{name}, keep at it never give up"
+# end
+
+# greetings("Daniel")
+
+# def add_and_log(num1, num2)
+#     puts num1 + num2
+#   end
+  
+#   def add_and_return(num1, num2)
+#     num1 + num2
+#   end
+  
+#   sum1 = add_and_log(2, 2)
+#   # => nil
+#   sum2 = add_and_return(2, 2)
+#   # => 4    
+
+# pp [1,2,3,4]
+
+# p [1,2,3,4]
+
+# puts [
+#     {
+#     id: 4,
+#     name: "John",
+#     age: 50
+# },
+# {
+#     id: 4,
+#     name: "John",
+#     age: 50
+# }
+# ]
+
+# def stylish_painter
+#     best_hairstyle = "Bob Ross"
+#     return "Jean-Michel Basquiat"
+#     best_hairstyle
+#   end
+  
+#   stylish_painter
